feba754601ee8949f98eb781af59a47f51e7698b
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-12-27 03:57:09 +0000

    pylint: auto fixes

--- plugins/lastfm.py
+++ plugins/lastfm.py
@@ -8,6 +8,7 @@
 # Code re-written by @DeletedUser420 (telegram), github user as code-rgb
 
 import asyncio
+
 from userge import Config, Message, get_collection, userge
 from userge.lastfm import get_response, pcurl, tglst, user
 from userge.utils import rand_array
