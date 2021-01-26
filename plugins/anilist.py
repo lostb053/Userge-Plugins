ed253d094ff7b9b19a46045172e4256b279a5440
lostb053 <lostb053@gmail.com>
2021-01-26 18:27:42 +0530

    k

--- plugins/anilist.py
+++ plugins/anilist.py
@@ -525,7 +525,7 @@ async def character_search(message: Message):
     (`{name}`)
 **ID:** {id_}
 
-**Featured in:** __{snin}__
+**Featured in:** `{snin}`
 [About Character]({url_})
 [Visit Website]({site_url})"""
 
