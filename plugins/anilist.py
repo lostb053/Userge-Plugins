b6a81c18a3156da6a422f17dd2d9cadf13aaa929
LostB053 <72547660+lostb053@users.noreply.github.com>
2021-03-27 10:56:38 +0530

    -_-

--- plugins/anilist.py
+++ plugins/anilist.py
@@ -691,7 +691,7 @@ async def get_ani(vars_):
         prql_id += "None"
     if sql_id=="":
         sql_id += "None"
-    additional = f"{prql}{sql}"
+    additional = f"{prql}\n{sql}"
     bannerImg = data.get("bannerImage")
     dura = f"\n➤ **DURATION:** `{duration} min/ep`" if duration!=None else ""
     charlist = []
